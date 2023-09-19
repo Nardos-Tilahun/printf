568961283f4fc364ecb50469735d2f2998cbd0da
Narrdos-Tilahun <contactnardos@gmail.com>
2023-09-19 13:18:12 -0700

    0-task 6th version

--- _printf.c
+++ _printf.c
@@ -1,40 +1,6 @@
 #include "main.h"
 
 /**
- * handle_conv - handle the conversion specifier
- * @format: constant character pointer for format
- * @print_args : variadic list of arguments
- * Return: the number of characters printed
- */
-int handle_conv(const char *format, va_list print_args)
-{
-	int count = 0, strlength = 0;
-	char temp, *strtemp;
-
-	if (*format == '\0')
-		return (-1);
-	if (*format == 'c')
-	{
-		temp = va_arg(print_args, int);
-		write(1, &temp, 1);
-		count++;
-	}
-	if (*format == 's')
-	{
-		strtemp = va_arg(print_args, char *);
-		for (; *strtemp != '\0' && strtemp != NULL; strlength++, strtemp++)
-			write(1, strtemp, 1);
-		count += strlength;
-	}
-	if (*format == '%')
-	{
-		write(1, format, 1);
-		count++;
-	}
-	return (count);
-}
-
-/**
  * _printf - produce output according to a format
  * @format: constant character pointer for format
  * Return: the number of characters printed
@@ -48,7 +14,7 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 	va_start(print_args, format);
-	for (; (*format != '\0'); format++)
+	for (; *format != '\0'; format++)
 	{
 		if (*format != '%')
 		{
@@ -58,9 +24,28 @@ int _printf(const char *format, ...)
 		else
 		{
 			format++;
-			count += handle_conv(format, print_args);
+			if (*format == '\0')
+				return (-1);
+			if (*format == 'c')
+			{
+				temp = va_arg(print_args, int);
+				write(1, &temp, 1);
+				count++;
+			}
+			if (*format == 's')
+			{
+				strtemp = va_arg(print_args, char *);
+				for (; *strtemp != '\0' && strtemp != NULL; strlength++, strtemp++)
+					write(1, strtemp, 1);
+				count += strlength;
+			}
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				count++;
+			}
 		}
+		va_end(print_args);
+		return (count);
 	}
-	va_end(print_args);
-	return (count);
 }
